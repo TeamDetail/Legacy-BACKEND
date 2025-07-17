c51a455f1c37c7aaad792f1dd07d7ae9902931c9
ENCAIL <siwon0876@gmail.com>
2025-07-17 16:22:46 +0900

    fix : 커스텀 예외처리 사용 & 퀴즈 리셋 기능 추가

--- src/main/java/com/learnmore/legacy/domain/card/service/CardService.java
+++ src/main/java/com/learnmore/legacy/domain/card/service/CardService.java
@@ -9,12 +9,15 @@ import com.learnmore.legacy.domain.card.presentation.dto.request.LineAttributeRe
 import com.learnmore.legacy.domain.card.presentation.dto.request.NationAttributeReq;
 import com.learnmore.legacy.domain.card.presentation.dto.request.RegionAttributeReq;
 import com.learnmore.legacy.domain.card.presentation.dto.response.*;
+import com.learnmore.legacy.domain.quiz.error.QuizError;
 import com.learnmore.legacy.domain.quiz.model.QuizHistory;
 import com.learnmore.legacy.domain.quiz.model.repo.QuizHistoryJpaRepo;
+import com.learnmore.legacy.domain.ruins.error.RuinsError;
 import com.learnmore.legacy.domain.ruins.model.Ruins;
 import com.learnmore.legacy.domain.ruins.model.repo.RuinsJpaRepo;
 import com.learnmore.legacy.domain.user.model.User;
 import com.learnmore.legacy.domain.user.model.repo.UserJpaRepo;
+import com.learnmore.legacy.global.exception.CustomException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -75,19 +78,19 @@ public class CardService {
     @Transactional
     public CardRes addCard(CardReq cardReq) {
         NationAttribute nation = nationAttributeJpaRepo.findByAttributeName(cardReq.getNationAttributeName())
-                .orElseThrow(() -> new EntityNotFoundException(CardError.NATION_ATTRIBUTE_NOT_FOUND.getMessage()));
+                .orElseThrow(() -> new CustomException(CardError.NATION_ATTRIBUTE_NOT_FOUND));
 
         LineAttribute line = lineAttributeJpaRepo.findByAttributeName(cardReq.getLineAttributeName())
-                .orElseThrow(() -> new EntityNotFoundException(CardError.LINE_ATTRIBUTE_ERROR.getMessage()));
+                .orElseThrow(() -> new CustomException(CardError.LINE_ATTRIBUTE_ERROR));
 
         RegionAttribute region = regionAttributeJpaRepo.findByAttributeName(cardReq.getRegionAttributeName())
-                .orElseThrow(() -> new EntityNotFoundException(CardError.REGION_ATTRIBUTE_ERROR.getMessage()));
+                .orElseThrow(() -> new CustomException(CardError.REGION_ATTRIBUTE_ERROR));
 
         QuizHistory quizHistory = quizHistoryJpaRepo.findById(cardReq.getQuizHistoryId())
-                .orElseThrow(() -> new EntityNotFoundException("퀴즈 내역을 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(QuizError.QUIZ_NOT_FOUND));
 
         Ruins ruins = ruinsJpaRepo.findByName(cardReq.getCardName())
-                .orElseThrow(() -> new EntityNotFoundException("유물을 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(RuinsError.RUINS_NOT_FOUND));
 
         Card card = Card.builder()
                 .cardName(cardReq.getCardName())
