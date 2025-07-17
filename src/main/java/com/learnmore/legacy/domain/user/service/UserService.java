011a709e03a0fa3ff2a9cff3ace1898678f8e52f
ganggun <165283835+ganggun@users.noreply.github.com>
2025-07-17 14:47:52 +0900

    refactor: 유저 서비스 칭호 서비스 분리

--- src/main/java/com/learnmore/legacy/domain/user/service/UserService.java
+++ src/main/java/com/learnmore/legacy/domain/user/service/UserService.java
@@ -1,12 +1,8 @@
 package com.learnmore.legacy.domain.user.service;
 
-import com.learnmore.legacy.domain.user.error.StyleError;
-import com.learnmore.legacy.domain.user.model.Style;
 import com.learnmore.legacy.domain.user.model.User;
-import com.learnmore.legacy.domain.user.model.repo.StyleJpaRepo;
 import com.learnmore.legacy.domain.user.model.repo.UserJpaRepo;
 import com.learnmore.legacy.domain.user.error.UserError;
-import com.learnmore.legacy.domain.user.presentation.dto.response.UserStyleRes;
 import com.learnmore.legacy.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,7 +14,6 @@ import java.util.List;
 public class UserService {
 
     private final UserJpaRepo userJpaRepo;
-    private final StyleJpaRepo styleJpaRepo;
 
     public void saveUser(User user) {
         userJpaRepo.save(user);
@@ -33,36 +28,7 @@ public class UserService {
         return userJpaRepo.existsById(userId);
     }
 
-    //Style
-    public void saveStyle(Style style) {
-        styleJpaRepo.save(style);
-    }
-
-    public Style findEquipStyle (User user) {
-        return styleJpaRepo.findByUserAndIsEquipTrue(user)
-                .orElse(null);
-
-    }
-
-    public Style findStyleByUser(User user) {
-        return styleJpaRepo.findByUser(user)
-                .orElseThrow(() -> new CustomException(StyleError.STYLE_NOT_FOUND));
-    }
-
-    public List<UserStyleRes> findAllStyles(User user) {
-        return styleJpaRepo.findAllStyleDtoByUser(user);
-    }
-
-    public Style findByUserAndStyleId(User user,Long styleId) {
-        return styleJpaRepo.findByUserAndStyleId(user,styleId)
-                .orElseThrow(() -> new CustomException(StyleError.STYLE_NOT_FOUND));
-    }
-
-    public boolean existsEquippedStyle(User user) {
-        return styleJpaRepo.existsByUserAndIsEquipTrue(user);
-    }
-
-    public boolean existsStyleByUserAndName (User user,String name) {
-        return styleJpaRepo.existsByUserAndStyleName(user,name);
+    public List<User> blockRanking() {
+        return userJpaRepo.findTop100ByOrderByAllBlocksDesc();
     }
 }
