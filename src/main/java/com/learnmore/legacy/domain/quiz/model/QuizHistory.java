5ec359443ce65b5cd4e1a7374f345311daca6db8
gokim0207 <gokim0207@dgsw.hs.kr>
2025-08-26 21:36:01 +0900

    fix : db 수정

--- src/main/java/com/learnmore/legacy/domain/quiz/model/QuizHistory.java
+++ src/main/java/com/learnmore/legacy/domain/quiz/model/QuizHistory.java
@@ -1,5 +1,6 @@
 package com.learnmore.legacy.domain.quiz.model;
 
+import com.learnmore.legacy.domain.card.model.Card;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,10 @@ public class QuizHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long quizHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id", nullable = false)
+    private Card card;
 
     @Column(name = "user_id", nullable = false)
     private Long userId;
