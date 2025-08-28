c1b74a9cbe985d6bd44d5adcc549b0b8f7e927e7
encail <siwon0876@gmail.com>
2025-08-29 08:23:18 +0900

    feat: 별점 추가

--- src/main/java/com/learnmore/legacy/domain/ruins/presentation/dto/request/RuinsCommentReq.java
+++ src/main/java/com/learnmore/legacy/domain/ruins/presentation/dto/request/RuinsCommentReq.java
@@ -2,6 +2,7 @@ package com.learnmore.legacy.domain.ruins.presentation.dto.request;
 
 public record RuinsCommentReq(
         Long ruinsId,
+        Long rating,
         String comment
 ) {
 }
--- src/main/java/com/learnmore/legacy/domain/ruins/presentation/dto/response/RuinsCommentRes.java
+++ src/main/java/com/learnmore/legacy/domain/ruins/presentation/dto/response/RuinsCommentRes.java
@@ -3,10 +3,12 @@ package com.learnmore.legacy.domain.ruins.presentation.dto.response;
 import com.learnmore.legacy.domain.ruins.model.RuinsComment;
 
 public record RuinsCommentRes(
+        Long rating,
         String comment
 ) {
     public static RuinsCommentRes from(RuinsComment ruinsComment) {
         return new RuinsCommentRes(
+                ruinsComment.getRating(),
                 ruinsComment.getComment()
         );
     }
