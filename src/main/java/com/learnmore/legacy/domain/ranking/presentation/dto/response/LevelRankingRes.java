fdc1b5e77ab1cc5e98c90f5f96293d9e6d6dcc2b
gokim0207 <gokim0207@dgsw.hs.kr>
2025-09-15 13:33:22 +0900

    fix : 랭킹 response 수정

---
+++ src/main/java/com/learnmore/legacy/domain/ranking/presentation/dto/response/LevelRankingRes.java
@@ -0,0 +1,18 @@
+package com.learnmore.legacy.domain.ranking.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LevelRankingRes {
+    private String nickname;
+    private Integer level;
+    private Integer exp;
+    private String imageUrl;
+    private UserStyleRes userStyle;
+}
