5e7b2b55dff681d3558a8534beb7f2df9013cf83
encail <siwon0876@gmail.com>
2025-09-17 08:30:46 +0900

    fix: 퀴즈 정답 확인 수정

--- src/main/java/com/learnmore/legacy/domain/quiz/model/repo/QuizHistoryJpaRepo.java
+++ src/main/java/com/learnmore/legacy/domain/quiz/model/repo/QuizHistoryJpaRepo.java
@@ -3,12 +3,9 @@ package com.learnmore.legacy.domain.quiz.model.repo;
 import com.learnmore.legacy.domain.quiz.model.QuizHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface QuizHistoryJpaRepo extends JpaRepository<QuizHistory, Long> {
 
     boolean existsByUserIdAndQuizId(Long userId, Long quizId);
 
-    Optional<Long> deleteAllByUserId(Long userId);
-
+    void deleteByUserIdAndQuizId(Long userId, Long quizId);
 }
