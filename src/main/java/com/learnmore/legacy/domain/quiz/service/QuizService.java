61c685ee8f400ef7a85c864247fb77a43c32c152
encail <siwon0876@gmail.com>
2025-09-16 09:12:49 +0900

    fix: 퀴즈 정답 확인 코드 수정

--- src/main/java/com/learnmore/legacy/domain/quiz/service/QuizService.java
+++ src/main/java/com/learnmore/legacy/domain/quiz/service/QuizService.java
@@ -2,6 +2,8 @@ package com.learnmore.legacy.domain.quiz.service;
 
 import com.learnmore.legacy.domain.block.model.repo.BlockHistoryJpaRepo;
 import com.learnmore.legacy.domain.block.service.BlockService;
+import com.learnmore.legacy.domain.card.model.Card;
+import com.learnmore.legacy.domain.card.model.repo.CardJpaRepo;
 import com.learnmore.legacy.domain.quiz.error.QuizError;
 import com.learnmore.legacy.domain.quiz.model.Quiz;
 import com.learnmore.legacy.domain.quiz.model.QuizHistory;
@@ -38,6 +40,7 @@ public class QuizService {
     private final BlockService blockService;
     private final BlockHistoryJpaRepo blockHistoryJpaRepo;
     private final RuinsJpaRepo ruinsJpaRepo;
+    private final CardJpaRepo cardJpaRepo;
 
     @Transactional
     public QuizAddRes addQuiz(QuizAddReq req) {
@@ -115,8 +118,13 @@ public class QuizService {
             boolean isCorrect = quiz.getAnswerOption().equalsIgnoreCase(request.answerOption());
             results.add(new QuizAnswerResult(quiz.getQuizId(), isCorrect));
 
+
+            Card card = cardJpaRepo.findByRuins_RuinsId(quiz.getRuinsId())
+                    .orElseThrow(() -> new CustomException(RuinsError.RUINS_NOT_FOUND));
+
             if (isCorrect) {
                 quizHistoryJpaRepo.save(QuizHistory.builder()
+                        .card(card)
                         .userId(userId)
                         .quizId(quiz.getQuizId())
                         .build());
