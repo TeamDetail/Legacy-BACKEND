0f98d7fa9397644fd14febe77cd0182be2b9e218
gokim0207 <gokim0207@dgsw.hs.kr>
2025-09-18 17:46:32 +0900

    fix : 카드 지역명으로 조회 수정

--- src/main/java/com/learnmore/legacy/domain/card/model/repo/RegionAttributeJpaRepo.java
+++ src/main/java/com/learnmore/legacy/domain/card/model/repo/RegionAttributeJpaRepo.java
@@ -1,12 +1,16 @@
 package com.learnmore.legacy.domain.card.model.repo;
 
 import com.learnmore.legacy.domain.card.model.RegionAttribute;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface RegionAttributeJpaRepo extends JpaRepository<RegionAttribute, Long> {
-    Optional<RegionAttribute> findByAttributeName(String attributeName);
+    @Query("SELECT r.regionAttributeId FROM RegionAttribute r WHERE r.attributeName = :attributeName")
+    Long findIdByAttributeName(@Param("attributeName") String attributeName);
 }
+
