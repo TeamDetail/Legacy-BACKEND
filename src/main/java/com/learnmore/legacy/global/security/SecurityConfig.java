1f356de0bc3a864eed88cf0e23d34d6cb25c73dc
gokim0207 <gokim0207@dgsw.hs.kr>
2025-09-30 21:42:52 +0900

    fix : 애플 에러 수정

--- src/main/java/com/learnmore/legacy/global/security/SecurityConfig.java
+++ src/main/java/com/learnmore/legacy/global/security/SecurityConfig.java
@@ -63,7 +63,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**", "/api-docs").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/sign-in", "/auth/sign-up", "/auth/refresh").permitAll()
-                        .requestMatchers("/apple/**").permitAll()
                         //user
                         .requestMatchers(HttpMethod.GET,"/user/uploadUrl").permitAll()
                         .requestMatchers("/user/**").hasAnyRole( "USER","ADMIN")
@@ -77,6 +76,8 @@ public class SecurityConfig {
                         .requestMatchers("/mail/**").hasAnyRole("USER", "ADMIN")
                         //kakao-auth
                         .requestMatchers("/kakao/**").permitAll()
+                        //apple-auth
+                        .requestMatchers("/apple/**").permitAll()
                         //inventory
                         .requestMatchers("/inventory/**").hasAnyRole("USER", "ADMIN")
                         //course
