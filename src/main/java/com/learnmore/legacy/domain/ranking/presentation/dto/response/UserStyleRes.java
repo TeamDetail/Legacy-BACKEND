d1029ada7119e42c569caf8e95b5af6355826140
ganggun <165283835+ganggun@users.noreply.github.com>
2025-10-21 21:12:42 +0900

    fix : 칭호장착 쿼리변경

--- src/main/java/com/learnmore/legacy/domain/ranking/presentation/dto/response/UserStyleRes.java
+++ src/main/java/com/learnmore/legacy/domain/ranking/presentation/dto/response/UserStyleRes.java
@@ -5,15 +5,17 @@ import com.learnmore.legacy.domain.user.model.Style;
 public record UserStyleRes(
         String name,
         String content,
-        Long styleId
+        Integer styleId,
+        Long titleId
 ) {
     public static UserStyleRes from(Style style) {
         if (style == null) {
-            return new UserStyleRes( "", "",0L);
+            return new UserStyleRes( "", "", 0,0L);
         }
         return new UserStyleRes(
                 style.getStyle().getStyleName(),
                 style.getStyle().getStyleContent(),
+                style.getStyle().getGrade(),
                 style.getStyle().getUserStyleId()
         );
     }
