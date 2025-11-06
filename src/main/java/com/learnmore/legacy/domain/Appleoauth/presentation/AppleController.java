31a4aab4ab96d1cc4f3530a609b1f3ca34e3d5d4
encail <siwon0876@gmail.com>
2025-11-06 09:07:53 +0900

    refactor: 애플 컨트롤러 태그 수정

--- src/main/java/com/learnmore/legacy/domain/Appleoauth/presentation/AppleController.java
+++ src/main/java/com/learnmore/legacy/domain/Appleoauth/presentation/AppleController.java
@@ -8,6 +8,7 @@ import com.learnmore.legacy.domain.auth.presentation.dto.response.TokenRes;
 import com.learnmore.legacy.global.common.dto.BaseResponse;
 import com.learnmore.legacy.global.common.repo.UserSessionHolder;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "APPLE", description = "APPLE OAuth API")
 @RestController
 @RequestMapping("/apple")
 @RequiredArgsConstructor
