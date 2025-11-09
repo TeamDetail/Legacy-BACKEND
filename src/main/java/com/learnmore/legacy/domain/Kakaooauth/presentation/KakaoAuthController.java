3d1a5dbd05ca2bb165cb23d8bc33481766d4ea9a
encail <siwon0876@gmail.com>
2025-11-10 08:23:18 +0900

    fix: 오타 수정

--- src/main/java/com/learnmore/legacy/domain/Kakaooauth/presentation/KakaoAuthController.java
+++ src/main/java/com/learnmore/legacy/domain/Kakaooauth/presentation/KakaoAuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "KAKAKO", description = "KAKAO OAuth API")
+@Tag(name = "KAKAO", description = "KAKAO OAuth API")
 @RestController
 @RequestMapping("/kakao")
 @RequiredArgsConstructor
